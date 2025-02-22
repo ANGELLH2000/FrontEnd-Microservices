26e4078f85c15404c1a47571c0b5b8aa6d329e66
Angel Leon <angeleonh2000@gmail.com>
2025-02-22 17:56:31 -0500

    Estructura Grid

---
+++ src/components/Chat/Chat.jsx
@@ -0,0 +1,9 @@
+import styles from "./Chat.module.css";
+const Chat = () => {
+    return (
+        <div className={styles.chat}>
+            <p>Hola</p>
+        </div>
+    );
+};
+export default Chat;
---
+++ src/components/SideBar/SideBar.jsx
@@ -0,0 +1,6 @@
+const SideBar = () => {
+    return (
+        <div >SideBar componentes</div>
+    );
+}
+export default SideBar;
